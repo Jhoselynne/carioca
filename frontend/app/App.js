08bb9aef1b70adae21f8e757e69d8bb4de839d98
Jhoselynne <jhoselynne@hotmail.com>
2021-12-28 20:18:50 +0100

    Fetch json data and present it

--- frontend/app/App.js
+++ frontend/app/App.js
@@ -1,10 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function App() {
+  const [cocktail, setCocktail] = useState([]);
+
+  useEffect(() => {
+  fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
+  // fetch('https://illanes.com/carioca/test_scoreboard.php')
+    .then((response) => response.json())
+    .then((json) => { setCocktail(json)
+      console.log(json);
+    })
+    .catch((e) => console.log(e));
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <FlatList
+        data={cocktail.drinks}
+        keyExtractor={item => item.idDrink}
+        renderItem={({item, index}) => (
+          <Text>{index + ': ' + item.idDrink + ' ' + item.strDrink}</Text>
+        )}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
