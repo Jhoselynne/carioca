cb89073faf5a43eb9d24b07bce09a937a8d83dbe
Christian Illanes <christian.illanes@gmail.com>
2021-12-30 23:26:16 +0100

    Add sql script to create tables and populate them. Add endpoint to list available endpoints.

--- backend/app/home/api/routes/web.php
+++ backend/app/home/api/routes/web.php
@@ -24,6 +24,51 @@ $router->post('login', function () {
     return "Login functionality coming soon here!";
 });
 
+$router->get('endpoint', function () {
+    $html = '
+        <!doctype html>
+
+        <html lang="en">
+            <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <title>Carioca endpoints</title>
+            </head>
+            <body>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>HOST</td>
+                            <td>https://illanes.com</td>
+                        </tr>
+                        <tr>
+                            <td>GET</td>
+                            <td>/carioca/api/public/user</td>
+                        </tr>
+                        <tr>
+                            <td>GET</td>
+                            <td>/carioca/api/public/user/{id}</td>
+                        </tr>
+                        <tr>
+                            <td>GET</td>
+                            <td>/carioca/api/public/round</td>
+                        </tr>
+                        <tr>
+                            <td>GET</td>
+                            <td>/carioca/api/public/round/{id}</td>
+                        </tr>
+                        <tr>
+                            <td>POST</td>
+                            <td>/carioca/api/public/login</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </body>
+        </html>
+    ';
+    return $html;
+});
+
 $router->get('scoreboard', function () {
     $carioca = new Carioca();
     array_push($carioca->scoreBoard, new User("Alex", 175));
@@ -36,16 +81,27 @@ $router->get('scoreboard', function () {
 });
 
 $router->get('user[/{id:[0-9]+}]', function ($id = null) {
-    $results = 'user called with id = \'' . $id . '\'';
+    $table = 'carioca_user';
     if (!empty($id)) {
-        $results = app('db')->select("SELECT id, name FROM user WHERE id = $id");
+        $results = app('db')->select("SELECT id, name FROM $table WHERE id = $id");
     } else {
-        $results = app('db')->select("SELECT id, name FROM user");
+        $results = app('db')->select("SELECT id, name FROM $table");
+    }
+    return $results;
+});
+
+$router->get('round[/{id:[0-9]+}]', function ($id = null) {
+    $table = 'carioca_round';
+    if (!empty($id)) {
+        $results = app('db')->select("SELECT id, name FROM $table WHERE id = $id");
+    } else {
+        $results = app('db')->select("SELECT id, name FROM $table");
     }
     return $results;
 });
 
 // Catch all route
 $router->get('[{path:.*}]', function ($path = null) use ($router) {
-    return $router->app->version() . '<br /><br />path \'' . $path . '\' was not found.';
+    // return $router->app->version() . '<br /><br />path \'' . $path . '\' was not found.';
+    return 'Path \'' . $path . '\' was not found.';
 });
--- backend/app/home/flyway/sql/V1__Initial.sql
+++ backend/app/home/flyway/sql/V1__Initial.sql
@@ -1,16 +1,60 @@
-CREATE TABLE user (
+CREATE TABLE carioca_user (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
     PRIMARY KEY (id)
 );
 
-INSERT INTO `user` (`name`) VALUES ('Christian');
-INSERT INTO `user` (`name`) VALUES ('Jhoselynne');
-INSERT INTO `user` (`name`) VALUES ('Yasemin');
-INSERT INTO `user` (`name`) VALUES ('Marta');
-INSERT INTO `user` (`name`) VALUES ('Alex');
-INSERT INTO `user` (`name`) VALUES ('Guest1');
-INSERT INTO `user` (`name`) VALUES ('Guest2');
-INSERT INTO `user` (`name`) VALUES ('Guest3');
-INSERT INTO `user` (`name`) VALUES ('Guest4');
-INSERT INTO `user` (`name`) VALUES ('Guest5');
+CREATE TABLE carioca_round (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE carioca_game (
+    id INT NOT NULL AUTO_INCREMENT,
+    ongoing INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (ongoing) REFERENCES carioca_round(id)
+);
+
+CREATE TABLE carioca_score (
+    game_id INT,
+    user_id INT,
+    round_id INT,
+    points INT,
+    PRIMARY KEY (game_id, user_id, round_id),
+    FOREIGN KEY (game_id) REFERENCES carioca_game(id),
+    FOREIGN KEY (user_id) REFERENCES carioca_user(id),
+    FOREIGN KEY (round_id) REFERENCES carioca_round(id)
+);
+
+INSERT INTO `carioca_user` (`name`) VALUES ('Christian');
+INSERT INTO `carioca_user` (`name`) VALUES ('Jhoselynne');
+INSERT INTO `carioca_user` (`name`) VALUES ('Yasemin');
+INSERT INTO `carioca_user` (`name`) VALUES ('Marta');
+INSERT INTO `carioca_user` (`name`) VALUES ('Alex');
+INSERT INTO `carioca_user` (`name`) VALUES ('Guest1');
+INSERT INTO `carioca_user` (`name`) VALUES ('Guest2');
+INSERT INTO `carioca_user` (`name`) VALUES ('Guest3');
+INSERT INTO `carioca_user` (`name`) VALUES ('Guest4');
+INSERT INTO `carioca_user` (`name`) VALUES ('Guest5');
+
+INSERT INTO `carioca_round` (`name`) VALUES ('2 trios');
+INSERT INTO `carioca_round` (`name`) VALUES ('1 trio 1 scale');
+INSERT INTO `carioca_round` (`name`) VALUES ('2 scales');
+INSERT INTO `carioca_round` (`name`) VALUES ('3 trios');
+INSERT INTO `carioca_round` (`name`) VALUES ('2 trios 1 scale');
+INSERT INTO `carioca_round` (`name`) VALUES ('1 trio 2 scales');
+INSERT INTO `carioca_round` (`name`) VALUES ('4 trios');
+INSERT INTO `carioca_round` (`name`) VALUES ('3 scales');
+
+INSERT INTO `carioca_game` (`ongoing`) VALUES (1);
+INSERT INTO `carioca_game` (`ongoing`) VALUES (1);
+INSERT INTO `carioca_game` (`ongoing`) VALUES (1);
+INSERT INTO `carioca_game` (`ongoing`) VALUES (1);
+INSERT INTO `carioca_game` (`ongoing`) VALUES (1);
+INSERT INTO `carioca_game` (`ongoing`) VALUES (1);
+INSERT INTO `carioca_game` (`ongoing`) VALUES (1);
+INSERT INTO `carioca_game` (`ongoing`) VALUES (1);
+INSERT INTO `carioca_game` (`ongoing`) VALUES (1);
+INSERT INTO `carioca_game` (`ongoing`) VALUES (1);
