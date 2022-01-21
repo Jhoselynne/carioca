cb40448b3e1874d77bc7b0259eceb6c9951d3877
Jhoselynne <83884939+Jhoselynne@users.noreply.github.com>
2022-01-21 04:24:21 +0100

    Renamed ChooseGameID to GameID

--- frontend/app/src/GameID.js
+++ frontend/app/src/GameID.js
@@ -4,7 +4,7 @@ import { Button, Text, StyleSheet, TextInput } from "react-native";
 import { View } from "react-native-web";
 import { ContextToken } from "../App";
 
-function ChooseGameID ({ navigation }) {
+function GameID ({ navigation }) {
     const {token} = useContext(ContextToken);
 
     return(
@@ -39,4 +39,11 @@ const styles = StyleSheet.create({
     }
 });
 
-export default ChooseGameID;
+export default GameID;
+
+// for (let index = 0; index < users.length; index++) {
+//     const user = users[index];
+//     for (let index2 = 0; index2 < user.points.length; index2++) {
+//       const points = user.points[index2];
+//     }
+//   }
